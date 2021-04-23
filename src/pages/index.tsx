66af61dac3f948424a404124c8318c4f4e93e3dc
Diogo Pocrifka <diogopocrifka@gmail.com>
2021-04-23 12:02:52 -0300

    Add lesson 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,31 @@
 // import { useEffect } from "react";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { format, parseISO } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 
-export default function Home(props) {
+import styles from "./home.module.scss";
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+};
+
+type HomeProps = {
+  lastestEpisodes: Episode[];
+  allEpisodes: Episode[];
+};
+
+export default function Home({ lastestEpisodes, allEpisodes }: HomeProps) {
   // SPA - Single-page Application (Works on ReactJS and NextJS)
   // useEffect(() => {
   //   fetch("http://localhost:3333/episodes")
@@ -8,7 +33,98 @@ export default function Home(props) {
   //     .then((data) => console.log(data));
   // }, []);
 
-  return <h1>Index</h1>;
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Lastest Episodes</h2>
+
+        <ul>
+          {lastestEpisodes.map((episode) => {
+            return (
+              <li key={episode.id}>
+                {/* 
+                  To make the Image component work
+                  we need to configure next.config.ts
+                  with the domains of our images
+                */}
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  {/*
+                    Link guarantees the SPA behavior
+                    only re-rendering what has changed
+                  */}
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a>{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/play-green.svg" alt="Play episode" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>All Episodes</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Members</th>
+              <th>Date</th>
+              <th>Duration</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map((episode) => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Play episode" />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
 }
 
 // SSR - Server-side Rendering (Works on NextJS)
@@ -27,14 +143,45 @@ export default function Home(props) {
 // SSG - Static Site Generation (Works on NextJS)
 // Runs at set intervals and stores the static html page
 // regardless of how many users are accessing this page
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  // Using fetch
+  // const response = await fetch(
+  //   "http://localhost:3333/episodes?_limit=12&_sort=published_at&_order=desc"
+  // );
+  // const data = await response.json();
+
+  // Using axios
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: enUS,
+      }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(episode.file.duration),
+      url: episode.file.url,
+    };
+  });
+
+  const lastestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
 
   return {
     props: {
-      episodes: data,
+      lastestEpisodes,
+      allEpisodes,
     },
-    revalidate: 60 * 60 * 8,
+    revalidate: 60 * 60 * 8, // 8 hours
   };
-}
+};
